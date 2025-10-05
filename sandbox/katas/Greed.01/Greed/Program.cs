eed60e8fe321a435643163ccaa6c5133beeb0816
IvaT <snufkins.hat@gmail.com>
2025-10-05 03:38:30 +0200

    Add extra credit extension (CalculateScoreExtra method)

--- sandbox/katas/Greed.01/Greed/Program.cs
+++ sandbox/katas/Greed.01/Greed/Program.cs
@@ -1,26 +1,17 @@
-﻿static int CalculateScore(IEnumerable<int> roll)
+﻿// Total score based on basic rules
+static int CalculateScore(IEnumerable<int> roll)
 {
-    // toto je vypůjčený nápad :-)
+    // zgrupování je vypůjčený nápad :-)
     var counts = roll.GroupBy(d => d).ToDictionary(g => g.Key, g => g.Count());
     int totalScore = 0;
 
-    // triples
-    foreach (var count in counts)
+    // triple
+    var triple = counts.FirstOrDefault(x => x.Value >= 3);
+
+    if (triple.Value >= 3)
     {
-        if (count.Value >= 3)
-        {
-
-            if (count.Key == 1)
-            {
-                totalScore = 1000;
-            }
-            else
-            {
-                totalScore = count.Key * 100;
-            }
-
-            counts[count.Key] -= 3;
-        }
+        totalScore = (triple.Key == 1) ? 1000 : triple.Key * 100;
+        counts[triple.Key] -= 3;
     }
 
     // singles
@@ -38,8 +29,62 @@
 }
 
 
+
+// Total score based on extra credit rules
+static int CalculateScoreExtra(IEnumerable<int> roll)
+{
+    var counts = roll.GroupBy(d => d).ToDictionary(g => g.Key, g => g.Count());
+    int totalScore = 0;
+
+    // straight
+    if (counts.Count == 6)
+    {
+        totalScore = 1200;
+    }
+
+    // three pairs
+    else if ((counts.Count == 3) && counts.All(x => x.Value == 2))
+    {
+        totalScore = 800;
+    }
+
+    // two triples
+    else if (counts.Count == 2 && counts.All(x => x.Value == 3))
+    {
+        totalScore = counts.Sum(i => i.Key == 1 ? i.Key * 1000 : i.Key * 100);
+    }
+
+    else
+    {
+        // triple+
+        var triple = counts.FirstOrDefault(x => x.Value >= 3);
+
+        if (triple.Value >= 3)
+        {
+            int interScore = (triple.Key == 1) ? 1000 : triple.Key * 100;
+            totalScore = interScore * (int)Math.Pow(2, triple.Value - 3);
+            counts[triple.Key] = 0;
+        }
+
+        // singles
+        if (counts.TryGetValue(1, out int remainingOnes))
+        {
+            totalScore += remainingOnes * 100;
+        }
+
+        if (counts.TryGetValue(5, out int remainingFives))
+        {
+            totalScore += remainingFives * 50;
+        }
+    }
+
+    return totalScore;
+}
+
+
+// input w/o validation (5 values in case of basic rules, 1-6 values for extra credit)
 int[] inputRoll = { 1, 1, 1, 5, 1 };
-int userScore = CalculateScore(inputRoll);
+int userScore = CalculateScoreExtra(inputRoll);
 
 string strRoll = string.Join(", ", inputRoll);
 Console.WriteLine($"The best score based on a given roll ({strRoll}): {userScore}");
