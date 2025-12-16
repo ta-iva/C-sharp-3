6a384858533285263c040de088e9d1000a5c9ff8
IvaT <snufkins.hat@gmail.com>
2025-12-16 13:12:07 +0100

    Add CategoryView

--- ToDoList_project/src/ToDoList.Frontend/Models/CategoryView.cs
+++ ToDoList_project/src/ToDoList.Frontend/Models/CategoryView.cs
@@ -2,15 +2,10 @@ namespace ToDoList.Frontend.Models;
 
 using System.ComponentModel.DataAnnotations;
 
-public class ToDoItemView
+public class CategoryView
 {
     public int Id { get; set; }
     [Required(ErrorMessage = "Name is mandatory.")]
     [StringLength(50, MinimumLength = 1)]
     public string Name { get; set; }
-    [Required(ErrorMessage = "Description is mandatory.")]
-    [StringLength(250)]
-    public string Description { get; set; }
-    public bool IsCompleted { get; set; }
-    public int CategoryId { get; set; }
 }
